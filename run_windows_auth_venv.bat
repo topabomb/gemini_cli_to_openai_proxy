48a5dc32a4e497d3de929c12c14e52f164d97651
topabomb <hualei.hb@gmail.com>
2025-09-12 12:27:49 +0800

    增加一个 auth的windows脚本

---
+++ run_windows_auth_venv.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Activating virtual environment and running the application...
+
+REM Change to the directory where this script is located
+cd /d "%~dp0"
+
+REM Activate the virtual environment
+call .\.venv\Scripts\activate.bat
+
+REM Run the Python script 
+echo Starting the Python application with 'auth' command and arguments: %*
+python -m gemini_cli_openaiapi_proxy auth %*
+echo Application has finished.
+pause
